8119033ec259cf95d39e0acc63dc3ed2530ad104
d0ubs <dubs.david@gmail.com>
2022-11-16 17:46:47 +0100

    New version

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -7,5 +7,5 @@ case "$1" in
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
     *.mkv | *.mp4 | *.avi) mediainfo "$1";;
-    *) highlight -O ansi "$1";;
+    *) highlight -O xterm256 "$1";;
 esac
